062dbb9f775193d56b0c4dcd04fdd9848326ff48
LuscasD <73210639+LuscasD@users.noreply.github.com>
2024-12-26 22:48:33 -0300

    UpdateGradle

--- ListaDeTarefas/app/build.gradle.kts
+++ ListaDeTarefas/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.listadetarefas"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.listadetarefas"
